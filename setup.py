31108b2760531fc02ee5df308b174b341e04123c
Vladimir Rusinov <vladimir@greenmice.info>
2010-02-17 16:31:22 +0300

    install script completed

--- setup.py
+++ setup.py
@@ -3,18 +3,27 @@
 from distutils.core import setup
 import os
 import glob
+import sys
 
-setup(name='ztc',
+#sys.prefix='/opt/ztc/'
+#sys.exec_prefix='/opt/ztc/'
+
+setup(
+      name='ztc',
       version = '10.2',
       description = 'Zabbix Template Collection',
       author = 'Vladimir Rusinov',
       author_email = 'vladimir@greenmice.info',
       url = 'http://trac.greenmice.info/ztc/',
       
+      home = '/opt/ztc/',
+      
       package_dir = {'': 'src'},
       packages = ['ztc', 'ztc.system'],
       
       scripts = glob.glob('src/*.py'),  
       
-      package_data={'ztc.system': ['data/*.dat']},
+      data_files=[
+            ('/etc/zabbix-agent.d/', ['conf/zabbix-agent.d/linux.conf', ]),
+      ],
 )
