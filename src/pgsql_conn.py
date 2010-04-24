a593cfa8ad6cd1e8d8c2536dadb6c3ee2090e6bb
Vladimir Rusinov <vladimir@greenmice.info>
2010-04-24 12:03:55 +0400

    pgsql.conn.* items

--- src/pgsql_conn.py
+++ src/pgsql_conn.py
@@ -6,7 +6,11 @@ Copyright (c) Vladimir Rusinov <vladimir@greenmice.info>
 
 Params:
     $1 - name of metric. Supported:
-        'idle_tnx' - number of clear buffers
+        'idle_tnx' - number of connections idle in transaction
+        'idle' - number of idle connections
+        'total' - total number of connections
+        'running' - number of backends running query
+        'waiting' - number of backends waiting
 '''
 
 import sys
@@ -21,6 +25,14 @@ metric = sys.argv[1]
 
 if metric == 'idle_tnx':
     q = "SELECT COUNT(*) FROM pg_stat_activity WHERE current_query = '<IDLE> in transaction'"
+elif metric == 'idle':
+    q = "SELECT COUNT(*) FROM pg_stat_activity WHERE current_query = '<IDLE>'"
+elif metric == 'total':
+    q = "SELECT COUNT(*) FROM pg_stat_activity"
+elif metric == 'running':
+    q = "SELECT COUNT(*) FROM pg_stat_activity WHERE current_query NOT LIKE '<IDLE%'"
+elif metric == 'waiting':
+    q = "SELECT COUNT(*) FROM pg_stat_activity WHERE waiting<>'f'"    
 else:
     notsupported("unknown metric")
 
