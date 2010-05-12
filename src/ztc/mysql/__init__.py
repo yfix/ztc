6e803f9afedbb5f6400b3f0fc751d1bc9f9e8dee
Vladimir Rusinov <vladimir@greenmice.info>
2010-05-12 20:49:25 +0400

    mysql.ping item

--- src/ztc/mysql/__init__.py
+++ src/ztc/mysql/__init__.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 """
 MySQL module for ZTC
+
+Copyright (c) 2010 Vladimir Rusinov <vladimir@greenmice.info>
 """
+
+import MySQLdb
 
 import ztc.commons
 
-class MySQL(object):
-    database='postgres'
+class MyDB(object):
+    database='mysql'
     host='localhost'
-    user='postgres'
+    user='root'
     password=''
+    
+    lasterr = None
     
     def __init__(self):
         self.config = ztc.commons.get_config('mysql')
@@ -21,4 +27,24 @@ class MySQL(object):
         self._connect()        
         
     def _connect(self):
-        pass
+        try:
+            self.conn =  MySQLdb.connect (host = self.host,
+                           user = self.user,
+                           passwd = self.password,
+                           db = self.database)
+            self.cursor = self.conn.cursor();
+        except:
+            self.conn = None
+            self.cursor = None
+    
+    def query(self, query):
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except Exception, e:
+            self.lasterr = e
+            return None
+
+if __name__ == '__main__':
+    m = MyDB()
+    print m.query("SELECT 1")
