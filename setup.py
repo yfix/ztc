2d7275606540178612f1db0366e1b36cdea7ddb1
Vladimir Rusinov <vladimir@greenmice.info>
2011-02-06 11:37:00 +0300

    fix: ztc.hw module not installing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 setup(
       name='ztc',
-      version = '10.11.1',
+      version = '10.11.2',
       description = 'Zabbix Template Collection',
       author = 'Vladimir Rusinov',
       author_email = 'vladimir@greenmice.info',
@@ -16,7 +16,7 @@ setup(
       home = '/opt/ztc/',
       
       package_dir = {'': 'src'},
-      packages = ['ztc', 'ztc.system', 'ztc.apache', 'ztc.mysql', 'ztc.nginx', 'ztc.vm', 'ztc.pgsql'],
+      packages = ['ztc', 'ztc.system', 'ztc.apache', 'ztc.mysql', 'ztc.nginx', 'ztc.vm', 'ztc.pgsql', 'ztc.hw'],
       
       scripts = glob.glob('src/*.py'),  
       
