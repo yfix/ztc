e75b8bf429ef3a70a005e71c4d7b7883d13c73fd
Vladimir Rusinov <vladimir@greenmice.info>
2012-01-17 20:01:21 +0400

    new version: 11.11.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 
 setup(
       name='ztc',
-      version = '11.11.1',
+      version = '11.11.2',
       description = 'Zabbix Template Collection',
       author = 'Vladimir Rusinov',
       author_email = 'vladimir@greenmice.info',
